676c688b28e5dad495cdeccc3a4a6c52b5dab6fa
anuragc2001 <chakraborty.anurag01@gmail.com>
2023-02-26 00:35:35 +0530

    added twilio

--- models/user.model.js
+++ models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    email: {
+    mobile: {
         type: String,
         required: true,
         unique: true
